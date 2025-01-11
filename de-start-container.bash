61efecfc991b7eeedb21caa711bfe52d87f0f201
Sean T. Allen <sean@seantallen.com>
2025-01-11 20:13:22 +0000

    Allow for using different containers

--- de-start-container.bash
+++ de-start-container.bash
@@ -4,12 +4,15 @@ set -e
 
 unset NAME
 unset WORKSPACE
+unset CONTAINER
 
-while getopts ":n:w:" opt; do
+while getopts ":n:w:c:" opt; do
   case $opt in
     n) NAME="$OPTARG"
     ;;
     w) WORKSPACE="$OPTARG"
+    ;;
+    c) CONTAINER="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1
@@ -35,4 +38,10 @@ if [[ -z "${WORKSPACE}" ]]; then
   exit 1
 fi
 
-docker run --user sean --name ${NAME} --mount src=${WORKSPACE},target=/workspace,type=bind -w /workspace --mount src=/home/sean/.ssh,target=/home/sean/.ssh,type=bind --mount src=/home/sean/.gitconfig,target=/home/sean/.gitconfig,type=bind --mount src=/home/sean/.gitconfig.local,target=/home/sean/.gitconfig.local,type=bind --mount src=/home/sean/.gnupg,target=/home/sean/.gnupg,type=bind --mount src=/home/sean/.config,target=/home/sean/.config,type=bind --mount src=/home/sean/.vim,target=/home/sean/.vim,type=bind --mount src=/home/sean/.vimrc,target=/home/sean/.vimrc,type=bind --mount src=/var/run/docker.sock,target=/var/run/docker.sock,type=bind --rm -i -t develop-with-pony:20250111 fish
+if [[ -z "${CONTAINER}" ]]; then
+  echo -e "\e[31mContainer to use needs to be set via -c."
+  echo -e "Exiting.\e[0m"
+  exit 1
+fi
+
+docker run --user sean --name ${NAME} --mount src=${WORKSPACE},target=/workspace,type=bind -w /workspace --mount src=/home/sean/.ssh,target=/home/sean/.ssh,type=bind --mount src=/home/sean/.gitconfig,target=/home/sean/.gitconfig,type=bind --mount src=/home/sean/.gitconfig.local,target=/home/sean/.gitconfig.local,type=bind --mount src=/home/sean/.gnupg,target=/home/sean/.gnupg,type=bind --mount src=/home/sean/.config,target=/home/sean/.config,type=bind --mount src=/home/sean/.vim,target=/home/sean/.vim,type=bind --mount src=/home/sean/.vimrc,target=/home/sean/.vimrc,type=bind --mount src=/var/run/docker.sock,target=/var/run/docker.sock,type=bind --rm -i -t ${CONTAINER} fish
