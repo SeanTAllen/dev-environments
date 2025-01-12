c39fa47c86dc78d2cc313b9cb1a0e3bdb58777d6
Sean T. Allen <sean@seantallen.com>
2025-01-12 02:09:55 +0000

    Update develop-with-pony image daily

--- develop-with-pony/build-and-push.sh
+++ develop-with-pony/build-and-push.sh
@@ -4,10 +4,14 @@ set -o errexit
 set -o nounset
 
 DOCKERFILE_DIR="$(dirname "$0")"
-
 NAME="ghcr.io/seantallen/dev-environment-develop-with-pony"
-TODAY=$(date +%Y%m%d)
-TAG_AS=${TODAY}
 
+# with date as tag
+TAG_AS=$(date +%Y%m%d)
+docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TAG_AS}"
+
+# with latest as tag
+TAG_AS="latest"
 docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
 docker push "${NAME}:${TAG_AS}"
