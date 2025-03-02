549f21786c1955ffe83a22ad6df26da0c7ed62ea
Sean T Allen <sean@seantallen.com>
2025-03-02 13:06:26 -0500

    Add "develop postgres" image (#18)

---
+++ develop-postgres/build-and-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+DOCKERFILE_DIR="$(dirname "$0")"
+NAME="ghcr.io/seantallen/dev-environment-develop-postgres"
+
+# with date as tag
+TAG_AS=$(date +%Y%m%d)
+docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TAG_AS}"
+
+# with latest as tag
+TAG_AS="latest"
+docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TAG_AS}"
